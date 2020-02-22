e39f296e8a87790878453bc14a0a1211aafec814
Nancy <nancyfzx666@qq.com>
2020-02-22 16:13:02 +0800

    update currying

--- utils/currying.js
+++ utils/currying.js
@@ -1,7 +1,7 @@
 /**
  * 函数柯里化
  */
-function curry(fn, args) {
+function currify(fn, args) {
     return function () {
         let _args = [].slice.call(arguments);
 
@@ -11,7 +11,7 @@ function curry(fn, args) {
 
         //fn.length是fn接收参数的个数，还不够的话继续递归
         if (_args.length < fn.length) {
-            return curry(fn, _args);
+            return currify(fn, _args);
         }
 
         //够了就传入fn调用
@@ -32,3 +32,45 @@ function sum(a, b, c) {
 }
 const fn = currying(sum);
 fn(1)(2)(3);
+
+
+//带占位符的
+function curry(fn, argLength = fn.length, holder = '_') {
+    return _curry(fn, argLength, holder, [], []);
+}
+function _curry(fn, length, holder, args = [], holderPos = []) {
+    return function(..._args) {
+        let pos = holderPos.slice();   //之前的占位符下标
+
+        _args.forEach(arg => {
+            if(arg !== holder) {
+                if(holderPos.length) {
+                    const p = holderPos.shift();
+                    pos.shift();
+                    args[p] = arg;
+                } else {
+                    args.push(arg);
+                }
+            } else {
+                args.push(arg);
+                pos.push(args.length - 1);
+            }
+        });
+
+        if(args.length >= length && args.slice(0, length).every(i => i !== holder)) {
+            return fn.apply(null, args);
+        } else {
+            return _curry(fn, length, holder, args, pos);
+        }
+    }
+}
+
+//test
+const fn1 = function(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+}
+const _fn = curry(fn1);
+_fn(1, 2, 3, 4, 5);
+_fn('_', 2, 3, 4, 5)(1);
+_fn(1, '_', '_', 4, 5)(2, 3);
+_fn(1, '_', 3)('_', 2,'_')(4)(5);
